b7670084721ed50348c109fbd31e0508fb73b97d
jasmirabueno <jasminemirabueno@gmail.com>
2015-09-14 21:48:16 +0800

    Changes for soft deletes

--- app/User.php
+++ app/User.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
@@ -36,6 +37,8 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+    
+    protected $dates = ['deleted_at'];
     
     public function role(){
          return $this->belongsTo('App\Role');
--- database/migrations/2015_09_06_090517_create_users_table.php
+++ database/migrations/2015_09_06_090517_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password', 60);
             $table->boolean('is_verified')->default(false);
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
             
